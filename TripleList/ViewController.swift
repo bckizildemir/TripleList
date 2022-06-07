0734a9d6b4a37228346546f0815f5976cc3ad760
bckizildemir <bckizildemir@gmail.com>
2022-06-07 17:22:29 +0300

    Initial Commit

---
+++ TripleList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TripleList
+//
+//  Created by Berke Can KIZILDEMİR on 7.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
